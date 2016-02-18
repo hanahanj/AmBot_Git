ed5477263985398c6038ccd4d095e72e03262da3
octopusgump <bohaozhang@wustl.edu>
2016-02-18 13:11:27 -0600

    added content to archive

--- js/archive.js
+++ js/archive.js
@@ -1,26 +1,211 @@
 $(document).ready(function(){
 
-$('#site_Milam').mouseover(function() {
-    $( "#title_Milam").css({"opacity": "1"});          
+//A
+$('#site_alorton').mouseover(function() {
+    $( "#title_alorton").css({"opacity": "1"});          
 });
-$('#site_Milam').mouseleave(function() {
-    $( "#title_Milam").css({"opacity": "0"});          
+$('#site_alorton').mouseleave(function() {
+    $( "#title_alorton").css({"opacity": "0"});          
+});
+$('#site_centralPort').mouseover(function() {
+    $( "#title_centralPort").css({"opacity": "1"});          
+});
+$('#site_centralPort').mouseleave(function() {
+    $( "#title_centralPort").css({"opacity": "0"});          
+});
+//A ends here
+
+//B
+$('#site_brass').mouseover(function() {
+    $( "#title_brass").css({"opacity": "1"});          
+});
+$('#site_brass').mouseleave(function() {
+    $( "#title_brass").css({"opacity": "0"});          
 });
 
+$('#site_brooklyn').mouseover(function() {
+    $( "#title_brooklyn").css({"opacity": "1"});          
+});
+$('#site_brooklyn').mouseleave(function() {
+    $( "#title_brooklyn").css({"opacity": "0"});          
+});
+//B ends here
 
-$('#site_CentralPort').mouseover(function() {
-    $( "#title_CentralPort").css({"opacity": "1"});          
+//C
+$('#site_canalOverlook').mouseover(function() {
+    $( "#title_canalOverlook").css({"opacity": "1"});          
 });
-$('#site_CentralPort').mouseleave(function() {
-    $( "#title_CentralPort").css({"opacity": "0"});          
+$('#site_canalOverlook').mouseleave(function() {
+    $( "#title_canalOverlook").css({"opacity": "0"});          
 });
 
-$('#site_NationalCity').mouseover(function() {
-    $( "#title_NationalCity").css({"opacity": "1"});          
+$('#site_chemetco').mouseover(function() {
+    $( "#title_chemetco").css({"opacity": "1"});          
 });
-$('#site_NationalCity').mouseleave(function() {
-    $( "#title_NationalCity").css({"opacity": "0"});          
+$('#site_chemetco').mouseleave(function() {
+    $( "#title_chemetco").css({"opacity": "0"});          
 });
+
+$('#site_collinsville').mouseover(function() {
+    $( "#title_collinsville").css({"opacity": "1"});          
+});
+$('#site_collinsville').mouseleave(function() {
+    $( "#title_collinsville").css({"opacity": "0"});          
+});
+
+$('#site_confluenceTower').mouseover(function() {
+    $( "#title_confluenceTower").css({"opacity": "1"});          
+});
+$('#site_confluenceTower').mouseleave(function() {
+    $( "#title_confluenceTower").css({"opacity": "0"});          
+});
+// C ends here
+
+//E
+$('#site_dupo').mouseover(function() {
+    $( "#title_dupo").css({"opacity": "1"});          
+});
+$('#site_dupo').mouseleave(function() {
+    $( "#title_dupo").css({"opacity": "0"});          
+});
+//E ends here
+
+//D
+$('#site_eaglecliffCemetery').mouseover(function() {
+    $( "#title_eaglecliffCemetery").css({"opacity": "1"});          
+});
+$('#site_eaglecliffCemetery').mouseleave(function() {
+    $( "#title_eaglecliffCemetery").css({"opacity": "0"});          
+});
+$('#site_eastCarondelet').mouseover(function() {
+    $( "#title_eastCarondelet").css({"opacity": "1"});          
+});
+$('#site_eastCarondelet').mouseleave(function() {
+    $( "#title_eastCarondelet").css({"opacity": "0"});          
+});
+// D ends here
+
+//F
+$('#site_fairmountParkRaceway').mouseover(function() {
+    $( "#title_fairmountParkRaceway").css({"opacity": "1"});          
+});
+$('#site_fairmountParkRaceway').mouseleave(function() {
+    $( "#title_fairmountParkRaceway").css({"opacity": "0"});          
+});
+//F ends here
+
+//G
+$('#site_gateway').mouseover(function() {
+    $( "#title_gateway").css({"opacity": "1"});          
+});
+$('#site_gateway').mouseleave(function() {
+    $( "#title_gateway").css({"opacity": "0"});          
+});
+//G ends here
+
+//H
+$('#site_horseshoeLake').mouseover(function() {
+    $( "#title_horseshoeLake").css({"opacity": "1"});          
+});
+$('#site_horseshoeLake').mouseleave(function() {
+    $( "#title_horseshoeLake").css({"opacity": "0"});          
+});
+//H ends here
+
+//K
+$('#site_Kaskaskia').mouseover(function() {
+    $( "#title_Kaskaskia").css({"opacity": "1"});          
+});
+$('#site_Kaskaskia').mouseleave(function() {
+    $( "#title_Kaskaskia").css({"opacity": "0"});          
+});
+//K ends here
+
+//L
+$('#site_lacledeSteel').mouseover(function() {
+    $( "#title_lacledeSteel").css({"opacity": "1"});          
+});
+$('#site_lacledeStell').mouseleave(function() {
+    $( "#title_lacledeSteel").css({"opacity": "0"});          
+});
+$('#site_lock27').mouseover(function() {
+    $( "#title_lock27").css({"opacity": "1"});          
+});
+$('#site_lcok27').mouseleave(function() {
+    $( "#title_lock27").css({"opacity": "0"});          
+});
+$('#site_lowlandForests').mouseover(function() {
+    $( "#title_lowlandForests").css({"opacity": "1"});          
+});
+$('#site_lowlandForests').mouseleave(function() {
+    $( "#title_lowlandForests").css({"opacity": "0"});          
+});
+//L ends here
+
+//M
+$('#site_mcKinley').mouseover(function() {
+    $( "#title_mcKinley").css({"opacity": "1"});          
+});
+$('#site_mcKinley').mouseleave(function() {
+    $( "#title_mcKinley").css({"opacity": "0"});          
+});
+$('#site_milam').mouseover(function() {
+    $( "#title_milam").css({"opacity": "1"});          
+});
+$('#site_milam').mouseleave(function() {
+    $( "#title_milam").css({"opacity": "0"});          
+});
+//M ends here
+
+
+//N
+$('#site_nationalCity').mouseover(function() {
+    $( "#title_nationalCity").css({"opacity": "1"});          
+});
+$('#site_nationalCity').mouseleave(function() {
+    $( "#title_nationalCity").css({"opacity": "0"});          
+});
+//N ends here
+
+//P
+$('#site_pumpStation').mouseover(function() {
+    $( "#title_pumpStation").css({"opacity": "1"});          
+});
+$('#site_pumpStation').mouseleave(function() {
+    $( "#title_pumpStation").css({"opacity": "0"});          
+});
+//P ends here
+
+//R
+$('#site_railroadDome').mouseover(function() {
+    $( "#title_railroadDome").css({"opacity": "1"});          
+});
+$('#site_railroadDome').mouseleave(function() {
+    $( "#title_railroadDome").css({"opacity": "0"});          
+});
+$('#site_refineryMuseum').mouseover(function() {
+    $( "#title_refineryMuseum").css({"opacity": "1"});          
+});
+$('#site_refineryMuseum').mouseleave(function() {
+    $( "#title_refineryMuseum").css({"opacity": "0"});          
+});
+//R ends here
+
+//S
+$('#site_sauget').mouseover(function() {
+    $( "#title_sauget").css({"opacity": "1"});          
+});
+$('#site_sauget').mouseleave(function() {
+    $( "#title_sauget").css({"opacity": "0"});          
+});
+$('#site_southRoxana').mouseover(function() {
+    $( "#title_southRoxana").css({"opacity": "1"});          
+});
+$('#site_southRoxana').mouseleave(function() {
+    $( "#title_southRoxana").css({"opacity": "0"});          
+});
+//S ends here
+
 
 $('.home').mouseover('click touchstart', function() {
 	$('.archiveContain').animate({height: '77%', width: "100%", top: '17.5%', }), 300;
