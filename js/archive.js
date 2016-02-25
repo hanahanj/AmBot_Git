fadf14dcd908a2205d17a0d04ea448d9e05aa673
octopusgump <bohaozhang@wustl.edu>
2016-02-25 13:03:29 -0600

    added restyle.css and edited archive files

--- js/archive.js
+++ js/archive.js
@@ -286,13 +286,13 @@ $("#site_southRoxana").mouseleave(function(){
 
 $('.home').mouseover('click touchstart', function() {
     // $('#mapContainer').animate({height: '82.5%', width: "100%", top: '17.5%', }), 300;
-    $(".menu").animate({opacity: '.9'}, 300); 
+    $(".menu").animate({opacity: '.9'}, 150); 
     $('.menu').css({"z-index": '100'}); 
 });
 
 $('.menu').mouseleave('click touchstart', function() {
     // $('#mapContainer').animate({top: '0%', left: "0%", height: '100%', width: "100%", }, 300);
-    $(".menu").animate({opacity: '0'}, 800); 
+    $(".menu").animate({opacity: '0'}, 500); 
     $('.menu').css({"z-index": '-10'});  
 });
 
