06951319e228e767a6f516e4c0b4cee78f5ae29a
Jonathan Hanahan <jonathan.hanahan@gmail.com>
2016-04-20 12:02:13 -0500

    removed path from itineraries home page maps

---
+++ js/itineraryOne_map_archive.js
@@ -0,0 +1,245 @@
+
+$(document).ready(function(){
+
+
+	jQuery( '#infowindow' ).hide();
+
+	function initialize() {
+
+		var styles = [
+		{
+			stylers: [ 
+			{ hue: "#ff0091" },
+			{ saturation: -100 }
+			]
+		},{
+			featureType: "road", 
+			elementType: "geometry",
+			stylers: [
+			{ lightness: 100 },
+			{ visibility: "simplified" }
+			]
+		},
+		{
+			featureType: "water", 
+			elementType: "geometry",
+			stylers: [
+			{ color: "#000000" },
+			// { lightness: 250 },
+			{ visibility: "simplified" }
+			]
+		},
+
+		{
+			featureType: "road",
+			elementType: "labels",
+			stylers: [
+         //Turns off road labels. 
+         { visibility: "off" }
+         ]
+     }
+     ];
+
+     var map_options = {
+     	center: new google.maps.LatLng(38.62529, -90.09554),
+     	mapTypeId: google.maps.MapTypeId.TERRAIN,
+     	zoom: 13,
+     	panControl: false,
+     	zoomControl: true,
+     	scaleControl: true,
+     	streetViewControl: false,
+     	styles: styles,
+     	mapTypeControl: true,
+     	mapTypeControlOptions: {
+     		mapTypeIds: [google.maps.MapTypeId.SATELLITE,google.maps.MapTypeId.TERRAIN],
+     		style: google.maps.MapTypeControlStyle.DEFAULT,
+     		position: google.maps.ControlPosition.TOP_RIGHT
+     	}
+     };
+
+     map = new google.maps.Map(document.getElementById("mapCanvas"), map_options);
+
+     map.setMapTypeId(google.maps.MapTypeId.TERRAIN);
+
+     var image = {
+     	url: '../images/ABMarker6.png',
+     };
+
+     var markers = [
+	    // Detailed information for each marker, where it is located and where it lineks to
+	    
+	    ['Frank Holten State Park', 38.59923, -90.09554, 'insert Site'],
+	    ['Lincoln Park', 38.60982, -90.15608, 'insert Site'],
+	    ['Sunken Garden', 38.62529, -90.14097, 'insert Site'],
+	    ['Jones Park', 38.62505, -90.12196, 'insert Site'],
+
+	    
+	    ];
+
+
+// INFO WINDOW
+var bounds = new google.maps.LatLngBounds();
+var infoWindow = new google.maps.InfoWindow();
+var gmarkers = [];
+var marker, i;
+
+for (i = 0; i < markers.length; i++) {
+	marker = new google.maps.Marker({
+		position: new google.maps.LatLng(markers[i][1], markers[i][2]),
+		map: map,
+		icon: image,
+		title: [0],
+		url:markers[i][3]
+	     		// zIndex: markers[i][3],
+	     	});
+	bounds.extend(marker.getPosition());
+
+
+//Infobox appear and disappear ///////////////////// 
+
+
+
+google.maps.event.addListener(marker, 'mouseover', (function (marker, i) {
+	return function () {  
+		jQuery( '#infowindow' ).show();
+		jQuery( '#infowindow' ).empty();
+		jQuery( '#infowindow' ).append( content[i][0] );					
+	}
+})(marker, i));
+gmarkers.push(marker);
+
+google.maps.event.addListener(marker, 'click', function() {
+	window.location.href = this.url;
+});
+
+google.maps.event.addListener(marker, 'mouseout', (function (marker, i) {
+	return function () {  
+		jQuery( '#infowindow' ).hide();					
+	}
+})(marker, i));
+gmarkers.push(marker);
+
+};
+
+};
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
+
+//Menu Move Map Canvas ///////////////////// 
+
+
+
+$('.menuFloatInt').mouseover(function() {
+	$(this).css({"font-style": "italic"});          
+});
+
+
+$('.menuFloatInt').mouseleave('click touchstart', function() {
+	$(this).css({"font-style": "normal"});          
+});
+
+
+
+$('.menuFloatSites').mouseover(function() {
+	$(this).css({"font-style": "italic"});          
+});
+
+
+$('.menuFloatSites').mouseleave('click touchstart', function() {
+	$(this).css({"font-style": "normal"});    
+});
+
+$('.menuFloatAbout').mouseover(function() {
+	$(this).css({"font-style": "italic"});           
+});
+
+
+$('.menuFloatAbout').mouseleave('click touchstart', function() {
+	$(this).css({"font-style": "normal"});  
+});
+
+
+
+$('.menuFloatInt').on('click touchstart', function() { 
+	$('#mapContainer').animate({width: '80%', height: "100%", top: "0%"}, 300); 
+	$('#listItineraries').animate({opacity: '1'}, 800);  
+	$('.about').animate({opacity: '0'}, 300);   
+	$('.logo').animate({opacity: '0'}, 300); 
+	$('#sitesContain').animate({opacity: '0'}, 300);
+	$('.close').animate({opacity: '1'}, 300);
+
+	$(this).css({"text-decoration":"underline", "font-style": "italic"});
+	$('.menuFloatSites').css({"text-decoration":"none", "font-style": "normal"}); 
+	$('.menuFloatAbout').css({"text-decoration":"none", "font-style": "normal"});   
+});
+
+$('.menuFloatSites').on('click touchstart', function() {
+	$('#mapContainer').animate({height: '80%', width: "100%", top: '0%'}, 300);  
+	$('#sitesContain').animate({opacity: '1'}, 800);
+	$('.about').animate({opacity: '0'}, 300);   
+	$('.logo').animate({opacity: '0'}, 300); 
+	$('.close').animate({opacity: '1'}, 300);
+
+	$(this).css({"text-decoration":"underline", "font-style": "italic"});
+	$('.menuFloatInt').css({"text-decoration":"none", "font-style": "normal"});
+	$('.menuFloatAbout').css({"text-decoration":"none", "font-style": "normal"});     
+});
+
+
+
+$('.menuFloatAbout').on('click touchstart', function() {
+	$('#mapContainer').animate({height: '75%', width: "100%", top: '25.5%', }), 300;
+	$('#listItineraries').animate({opacity: '0'}, 300);
+	$('.about').animate({opacity: '1'}, 300);   
+	$('.logo').animate({opacity: '1'}, 300); 
+	$('#sitesContain').animate({opacity: '0'}, 300);
+	$('.close').animate({opacity: '1'}, 300);
+
+	$(this).css({"text-decoration":"underline", "font-style": "italic"}); 
+	$('.menuFloatInt').css({"text-decoration":"none", "font-style": "normal"}); 
+	$('.menuFloatSites').css({"text-decoration":"none", "font-style": "normal"});
+
+});
+
+$('.close').on('click touchstart', function() { 
+	$('#mapContainer').animate({top: '0%', left: "0%", height: '100%', width: "100%", }, 300);
+	$(this).animate({opacity: '0'}, 300);  
+	$('#listItineraries').animate({opacity: '0'}, 300);
+	$('.about').animate({opacity: '0'}, 300);   
+	$('.logo').animate({opacity: '0'}, 300); 
+	$('#sitesContain').animate({opacity: '0'}, 300);
+	$('.menuFloatInt').css({"text-decoration":"none", "font-style": "normal"}); 
+	$('.menuFloatSites').css({"text-decoration":"none", "font-style": "normal"});
+	$('.menuFloatAbout').css({"text-decoration":"none", "font-style": "normal"}); 
+
+
+});
+
+
+$('.home').mouseover('click touchstart', function() {
+	// $('#mapContainer').animate({height: '82.5%', width: "100%", top: '17.5%', }), 300;
+	$(".menu").animate({opacity: '.9'}, 300); 
+	$('.menu').css({"z-index": '100'}); 
+});
+
+$('.menu').mouseleave('click touchstart', function() {
+	// $('#mapContainer').animate({top: '0%', left: "0%", height: '100%', width: "100%", }, 300);
+	$(".menu").animate({opacity: '0'}, 800); 
+	$('.menu').css({"z-index": '-10'});  
+});
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
---
+++ js/itineraryTwo_map_archive.js
@@ -0,0 +1,270 @@
+
+$(document).ready(function(){
+
+
+	jQuery( '#infowindow' ).hide();
+
+	function initialize() {
+
+		var styles = [
+		{
+			stylers: [ 
+			{ hue: "#ff0091" },
+			{ saturation: -100 }
+			]
+		},{
+			featureType: "road", 
+			elementType: "geometry",
+			stylers: [
+			{ lightness: 100 },
+			{ visibility: "simplified" }
+			]
+		},
+		{
+			featureType: "water", 
+			elementType: "geometry",
+			stylers: [
+			{ color: "#000000" },
+			// { lightness: 250 },
+			{ visibility: "simplified" }
+			]
+		},
+
+		{
+			featureType: "road",
+			elementType: "labels",
+			stylers: [
+         //Turns off road labels. 
+         { visibility: "off" }
+         ]
+     }
+     ];
+
+     var map_options = {
+     	center: new google.maps.LatLng(38.7051, -90.16234),
+     	mapTypeId: google.maps.MapTypeId.TERRAIN,
+     	zoom: 11,
+     	panControl: false,
+     	zoomControl: true,
+     	scaleControl: true,
+     	streetViewControl: false,
+     	styles: styles,
+     	mapTypeControl: true,
+     	mapTypeControlOptions: {
+     		mapTypeIds: [google.maps.MapTypeId.SATELLITE,google.maps.MapTypeId.TERRAIN],
+     		style: google.maps.MapTypeControlStyle.DEFAULT,
+     		position: google.maps.ControlPosition.TOP_RIGHT
+     	}
+     };
+
+     map = new google.maps.Map(document.getElementById("mapCanvas"), map_options);
+
+     map.setMapTypeId(google.maps.MapTypeId.TERRAIN);
+
+     var image = {
+     	url: '../images/ABMarker6.png',
+     };
+
+     var markers = [
+	    // Detailed information for each marker, where it is located and where it lineks to
+	    
+	    ['WoodRiver PP', 38.86878, -90.1326, 'insert Site'],
+	    ['Roxana Mound', 38.85366, -90.07574, 'insert Site'],
+	    ['Chemetco', 38.80113, -90.09743, 'insert Site'],
+	    ['American Steel', 38.7051, -90.16234, 'insert Site'],
+	    ['Venice', 38.67539, -90.17586, 'insert Site'],
+	    ['Milam', 38.65625, -90.12934, 'insert Site'],
+	    ['Sam Chucallo', 38.64385, -90.11559, 'insert Site'],
+	    ['Powell Mound', 38.65342, -90.09604, 'insert Site'],
+	    ['Cahokia', 38.65682, -90.06231, 'insert Site'],
+	    ['Alorton', 38.60379, -90.12042, 'insert Site'],
+	    ['Sauget', 38.60805, -90.18028, 'insert Site'],
+	    ['Mulch', 38.58463, -90.19097, 'insert Site'],
+	    ['Pulcher', 38.49603, -90.23869, 'insert Site'],
+
+	   
+	    
+	    ];
+
+
+var clusterStyles = [{
+	url: 'http://oi62.tinypic.com/zjbf9l.jpg',
+	height: 80,
+	width: 150,
+	textSize: 16,
+	backgroundPosition: "-4 0"
+}];
+
+var mcOptions = {
+	gridSize: 30,
+	styles: clusterStyles,
+	maxZoom: 15
+};
+
+var bounds = new google.maps.LatLngBounds();
+var infoWindow = new google.maps.InfoWindow();
+var gmarkers = [];
+var marker, i;
+
+for (i = 0; i < markers.length; i++) {
+	marker = new google.maps.Marker({
+		position: new google.maps.LatLng(markers[i][1], markers[i][2]),
+		map: map,
+		icon: image,
+		title: [0],
+		url:markers[i][3]
+	     		// zIndex: markers[i][3],
+	     	});
+	bounds.extend(marker.getPosition());
+
+
+//Infobox appear and disappear ///////////////////// 
+
+
+
+google.maps.event.addListener(marker, 'mouseover', (function (marker, i) {
+	return function () {  
+		jQuery( '#infowindow' ).show();
+		jQuery( '#infowindow' ).empty();
+		jQuery( '#infowindow' ).append( content[i][0] );					
+	}
+})(marker, i));
+gmarkers.push(marker);
+
+google.maps.event.addListener(marker, 'click', function() {
+	window.location.href = this.url;
+});
+
+google.maps.event.addListener(marker, 'mouseout', (function (marker, i) {
+	return function () {  
+		jQuery( '#infowindow' ).hide();					
+	}
+})(marker, i));
+gmarkers.push(marker);
+
+
+
+
+};
+var mc = new MarkerClusterer(map, gmarkers, mcOptions);
+map.fitBounds(bounds);
+};
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
+//Menu Move Map Canvas ///////////////////// 
+
+
+
+$('.menuFloatInt').mouseover(function() {
+	$(this).css({"font-style": "italic"});          
+});
+
+
+$('.menuFloatInt').mouseleave('click touchstart', function() {
+	$(this).css({"font-style": "normal"});          
+});
+
+
+
+$('.menuFloatSites').mouseover(function() {
+	$(this).css({"font-style": "italic"});          
+});
+
+
+$('.menuFloatSites').mouseleave('click touchstart', function() {
+	$(this).css({"font-style": "normal"});    
+});
+
+$('.menuFloatAbout').mouseover(function() {
+	$(this).css({"font-style": "italic"});           
+});
+
+
+$('.menuFloatAbout').mouseleave('click touchstart', function() {
+	$(this).css({"font-style": "normal"});  
+});
+
+
+
+$('.menuFloatInt').on('click touchstart', function() { 
+	$('#mapContainer').animate({width: '80%', height: "100%", top: "0%"}, 300); 
+	$('#listItineraries').animate({opacity: '1'}, 800);  
+	$('.about').animate({opacity: '0'}, 300);   
+	$('.logo').animate({opacity: '0'}, 300); 
+	$('#sitesContain').animate({opacity: '0'}, 300);
+	$('.close').animate({opacity: '1'}, 300);
+
+	$(this).css({"text-decoration":"underline", "font-style": "italic"});
+	$('.menuFloatSites').css({"text-decoration":"none", "font-style": "normal"}); 
+	$('.menuFloatAbout').css({"text-decoration":"none", "font-style": "normal"});   
+});
+
+$('.menuFloatSites').on('click touchstart', function() {
+	$('#mapContainer').animate({height: '80%', width: "100%", top: '0%'}, 300);  
+	$('#sitesContain').animate({opacity: '1'}, 800);
+	$('.about').animate({opacity: '0'}, 300);   
+	$('.logo').animate({opacity: '0'}, 300); 
+	$('.close').animate({opacity: '1'}, 300);
+
+	$(this).css({"text-decoration":"underline", "font-style": "italic"});
+	$('.menuFloatInt').css({"text-decoration":"none", "font-style": "normal"});
+	$('.menuFloatAbout').css({"text-decoration":"none", "font-style": "normal"});     
+});
+
+
+
+$('.menuFloatAbout').on('click touchstart', function() {
+	$('#mapContainer').animate({height: '75%', width: "100%", top: '25.5%', }), 300;
+	$('#listItineraries').animate({opacity: '0'}, 300);
+	$('.about').animate({opacity: '1'}, 300);   
+	$('.logo').animate({opacity: '1'}, 300); 
+	$('#sitesContain').animate({opacity: '0'}, 300);
+	$('.close').animate({opacity: '1'}, 300);
+
+	$(this).css({"text-decoration":"underline", "font-style": "italic"}); 
+	$('.menuFloatInt').css({"text-decoration":"none", "font-style": "normal"}); 
+	$('.menuFloatSites').css({"text-decoration":"none", "font-style": "normal"});
+
+});
+
+$('.close').on('click touchstart', function() { 
+	$('#mapContainer').animate({top: '0%', left: "0%", height: '100%', width: "100%", }, 300);
+	$(this).animate({opacity: '0'}, 300);  
+	$('#listItineraries').animate({opacity: '0'}, 300);
+	$('.about').animate({opacity: '0'}, 300);   
+	$('.logo').animate({opacity: '0'}, 300); 
+	$('#sitesContain').animate({opacity: '0'}, 300);
+	$('.menuFloatInt').css({"text-decoration":"none", "font-style": "normal"}); 
+	$('.menuFloatSites').css({"text-decoration":"none", "font-style": "normal"});
+	$('.menuFloatAbout').css({"text-decoration":"none", "font-style": "normal"}); 
+
+
+});
+
+
+$('.home').mouseover('click touchstart', function() {
+	// $('#mapContainer').animate({height: '82.5%', width: "100%", top: '17.5%', }), 300;
+	$(".menu").animate({opacity: '.9'}, 300); 
+	$('.menu').css({"z-index": '100'}); 
+});
+
+$('.menu').mouseleave('click touchstart', function() {
+	// $('#mapContainer').animate({top: '0%', left: "0%", height: '100%', width: "100%", }, 300);
+	$(".menu").animate({opacity: '0'}, 800); 
+	$('.menu').css({"z-index": '-10'});  
+});
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
