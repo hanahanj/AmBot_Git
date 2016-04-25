4aa2d46c703951a2d974475163e95aeaf8f58e03
Jonathan Hanahan <jonathan.hanahan@gmail.com>
2016-04-25 11:26:40 -0500

    fixed floating description issues when click on another menu option

--- js/streetviewToggle.js
+++ js/streetviewToggle.js
@@ -60,7 +60,9 @@ $('.menuFloatVisit').mouseleave('click touchstart', function() {
 
 $('.menuFloatStreetview').on('click touchstart', function() {
 	$('#bgContainer').animate({top: '6%', left: "0%", height: '88%', width: "100%", }, 300);
-	$('#bgContainer').animate({opacity: '1'}, 300); 
+	$('#bgContainer').animate({opacity: '1'}, 300);
+	$('.site_Title').animate({bottom: '0%',}, 300); 
+	$('.siteText').animate({top: '100%',}, 300); 
 	$('.streetView').animate({opacity: '1'}, 800); 
 	$('#listItineraries').animate({opacity: '0'}, 300);
 	$(this).css({"text-decoration":"underline", "font-style": "italic"});
@@ -101,7 +103,9 @@ $('.menuFloatDescription').on('click touchstart', function() {
 });
 
 $('.menuFloatInt').on('click touchstart', function() { 
-	$('#bgContainer').animate({top: '6%', left: "6%", height: '88%', width: "88%", }, 300); 
+	$('#bgContainer').animate({top: '6%', left: "6%", height: '88%', width: "88%", }, 300);
+	$('.site_Title').animate({bottom: '0%',}, 300); 
+	$('.siteText').animate({top: '100%',}, 300); 
 	$('#bgContainer').animate({opacity: '.5'}, 300); 
 	$('#listItineraries').animate({opacity: '1'}, 800);  
 	$('#listItineraries').css({"z-index": '10'});  
@@ -119,9 +123,9 @@ $('.menuFloatInt').on('click touchstart', function() {
 $('.menuFloatVisit').on('click touchstart', function() {  
 	$('#bgContainer').animate({width: '100%', height: "100%", top: "0%", left:"0%"}, 300);  
 	$('#bgContainer').animate({opacity: '1'}, 300); 
+	$('.site_Title').animate({bottom: '0%',}, 300); 
+	$('.siteText').animate({top: '100%',}, 300);
 	$('.directions').animate({opacity: '1'}, 800); 
-	$('.site_Title').animate({opacity: '0'}, 300); 
-	$('.site_Title').css({"z-index": '-10'}); 
 	$('.streetView').animate({opacity: '0'}, 300); 
 	$('#listItineraries').animate({opacity: '0'}, 300); 
 	$('.streetView').css({"z-index": '0'});
