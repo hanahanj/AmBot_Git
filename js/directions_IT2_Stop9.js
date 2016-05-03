9d22ff18c5a08313e7690b90bf0d4107e40960a5
levelland <jessevogler@gmail.com>
2016-05-03 11:46:57 +0400

    adjusted zoom

--- js/directions_IT2_Stop9.js
+++ js/directions_IT2_Stop9.js
@@ -122,7 +122,7 @@ var fType = getUrlVars()["q"];
 
      	// Update this location for Each site
 
-     	center: new google.maps.LatLng(  38.6607, -90.0621),	
+     	center: new google.maps.LatLng(  38.6603, -90.0621),	
      	zoom: 18,
      	styles: styles,
 
@@ -133,7 +133,7 @@ var fType = getUrlVars()["q"];
 
      	// Update this location for Each site
 
-     var Milam_Loc = new google.maps.LatLng(  38.6607, -90.0621);
+     var Milam_Loc = new google.maps.LatLng(  38.6603, -90.0621);
     var imageMark1 = '../images/ABMarker6.png';
 
      var Milam_Mark = new google.maps.Marker({
