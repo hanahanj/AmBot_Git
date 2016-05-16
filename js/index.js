4a1b95154f250b36f81df484b368abfa9559e1ef
Jonathan Hanahan <jonathan.hanahan@gmail.com>
2016-05-16 12:58:21 -0500

    updated menu to click and close

--- js/index.js
+++ js/index.js
@@ -396,18 +396,31 @@ function checkSplash(){
     };
 }
 
-$('.home').mouseover('click touchstart', function() {
+$('.home').click('click touchstart', function() {
 	$('.menu').animate({opacity: '1'}, 120); 
 	$('.menuLinks').animate({opacity: '1'}, 120); 
 	$('.menuLinks').css({"z-index": '-10'}, 120); 
-	$('.home').animate({opacity: '0'}, 120); 
+	$(this).animate({opacity: '0'}, 300);
+	$(this).css({"z-index": '1000'}, 120);
 
+	$('.homeClose').animate({opacity: '1'}, 300);
+	$('.homeClose').css({"z-index": '2000'}, 120); 
 });
 
-$('.menu').mouseleave('click touchstart', function() {
+$('.homeClose').click('click touchstart', function() {
+	$('.menu').animate({opacity: '0'}, 120); 
 	$('.menuLinks').animate({opacity: '0'}, 120); 
-	$('.home').animate({opacity: '1'}, 120); 
+	$('.menuLinks').css({"z-index": '-10'}, 120); 
+	$(this).animate({opacity: '0'}, 300);  
+	$(this).css({"z-index": '1000'}, 120);  
+	$('.home').animate({opacity: '1'}, 300);
+	$('.home').css({"z-index": '2000'}, 120); 
 });
+
+// $('.menu').mouseleave('click touchstart', function() {
+// 	$('.menuLinks').animate({opacity: '0'}, 120); 
+// 	$('.home').animate({opacity: '1'}, 120); 
+// });
 
 
 
