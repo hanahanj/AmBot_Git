0f37f300c1c83d5a404e53d7a7d39acdabfc536d
octopusgump <bohaozhang@wustl.edu>
2016-05-22 12:33:17 -0500

    changed menu title

--- js/streetviewToggle.js
+++ js/streetviewToggle.js
@@ -61,7 +61,7 @@ $('.menuFloatVisit').mouseleave('click touchstart', function() {
 $('.menuFloatStreetview').mouseover('click touchstart', function() {
 	$('#bgContainer').animate({top: '6%', left: "0%", height: '88%', width: "100%", }, 300);
 	$('#bgContainer').animate({opacity: '1'}, 300);
-	$('.site_Title').animate({bottom: '0%',}, 300); 
+	$('.site_Title').animate({bottom: '7%',}, 300); 
 	$('.siteText').animate({top: '100%',}, 300); 
 	$('.streetView').animate({opacity: '1'}, 800); 
 	$('.listItineraries').animate({opacity: '0'}, 300);
@@ -104,7 +104,7 @@ $('.menuFloatDescription').mouseover('click touchstart', function() {
 
 $('.menuFloatInt').mouseover('click touchstart', function() { 
 	$('#bgContainer').animate({top: '6%', left: "6%", height: '88%', width: "88%", }, 300);
-	$('.site_Title').animate({bottom: '0%',}, 300); 
+	$('.site_Title').animate({bottom: '7%',}, 300); 
 	$('.siteText').animate({top: '100%',}, 300); 
 	$('#bgContainer').animate({opacity: '.5'}, 300); 
 	$('.listItineraries').animate({opacity: '1'}, 800);  
@@ -123,7 +123,7 @@ $('.menuFloatInt').mouseover('click touchstart', function() {
 $('.menuFloatVisit').mouseover('click touchstart', function() {  
 	$('#bgContainer').animate({width: '100%', height: "100%", top: "0%", left:"0%"}, 300);  
 	$('#bgContainer').animate({opacity: '1'}, 300); 
-	$('.site_Title').animate({bottom: '0%',}, 300); 
+	$('.site_Title').animate({bottom: '7%',}, 300); 
 	$('.siteText').animate({top: '100%',}, 300);
 	$('.directions').animate({opacity: '1'}, 800); 
 	$('.streetView').animate({opacity: '0'}, 300); 
@@ -140,7 +140,7 @@ $('.menuFloatVisit').mouseover('click touchstart', function() {
 });
 
 $('.close').on('click touchstart', function() { 
-	$('.site_Title').animate({bottom: '0%',}, 300); 
+	$('.site_Title').animate({bottom: '7%',}, 300); 
 	$('.siteText').animate({top: '100%',}, 300);
 	$('#bgContainer').animate({top: '6%', left: "6%", height: '88%', width: "88%", }, 300); 
 	$('#bgContainer').animate({opacity: '1'}, 800); 
