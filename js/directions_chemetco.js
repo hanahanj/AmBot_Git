3437bbb5cf5e4934a60650245d05b6f8dbd3630c
octopusgump <bohaozhang@wustl.edu>
2016-05-23 00:14:12 -0500

    fixing responsive design for site pages

--- js/directions_chemetco.js
+++ js/directions_chemetco.js
@@ -3,13 +3,13 @@ $(document).ready(function(){
 	var directionsPanelIn = true;
 	$(".directionButton").click(function(){
 		if (directionsPanelIn == true){
-			$("#directionsPanel").animate({left: '-23%'}, 500); //1st variable indicate height or width; 2nd variable indicates time in millis
+			$("#directionsPanel").animate({left: '-330px'}, 500); //1st variable indicate height or width; 2nd variable indicates time in millis
 			$(".directionButton #directionClose1").css({transform: 'rotate(60deg)'}, 2000);
 			$(".directionButton #directionClose2").css({transform: 'rotate(-60deg)'}, 2000);
 			directionsPanelIn = false;
 		}
 		else{
-			$("#directionsPanel").animate({left: '7%'}, 500); //1st variable indicate height or width; 2nd variable indicates time in millis
+			$("#directionsPanel").animate({left: '25px'}, 500); //1st variable indicate height or width; 2nd variable indicates time in millis
 			$(".directionButton #directionClose1").css({transform: 'rotate(-60deg)'}, 2000);
 			$(".directionButton #directionClose2").css({transform: 'rotate(60deg)'}, 2000);
 			directionsPanelIn = true;
