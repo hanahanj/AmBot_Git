5726985f996b7e78c03666de497834f91e708b43
Jonathan Hanahan <jonathan.hanahan@gmail.com>
2016-05-31 15:41:44 -0500

    changed site page menu to allow hover changes between visit/view/directions and info appears on top of whatever is currently up

--- js/streetviewToggle.js
+++ js/streetviewToggle.js
@@ -57,13 +57,26 @@ $('.menuFloatVisit').mouseleave('click touchstart', function() {
 
 
 $('.menuFloatDescription').mouseover('click touchstart', function() { 
-	$('#bgContainer').animate({top: '6%', left: "6%", height: '88%', width: "88%", }, 300);
-	$('#bgContainer').animate({opacity: '1'}, 300);
+	// $('#bgContainer').animate({top: '6%', left: "6%", height: '88%', width: "88%", }, 300);
+	// $('#bgContainer').animate({opacity: '1'}, 300);
 	$('#bgContainer').animate({opacity: '0.5'}, 200);
 	$('.site_Title').animate({bottom: '78%',}, 300);   
 	$('.siteText').animate({top: '23%',}, 300);
 	$('.siteText').css({"z-index": '10'});
 	$('.close').animate({opacity: '1'}, 300); 
+	// $('.streetView').css({"z-index": '-10'});
+	// $('.directions').css({"z-index": '-10'});
+
+	$(this).css({"text-decoration":"underline", "font-style": "italic"});
+	$('.menuFloatStreetview').css({"text-decoration":"none", "font-style": "normal"});
+	$('.menuFloatInt').css({"text-decoration":"none", "font-style": "normal"});  
+	$('.menuFloatVisit').css({"text-decoration":"none", "font-style": "normal"});
+	$('.menuFloatView').css({"text-decoration":"none", "font-style": "normal"});
+});
+
+$('.menuFloatView').mouseover('click touchstart', function() { 
+	$('#bgContainer').animate({top: '6%', left: "6%", height: '88%', width: "88%", }, 300);
+	$('#bgContainer').animate({opacity: '1'}, 300);
 	$('.streetView').css({"z-index": '-10'});
 	$('.directions').css({"z-index": '-10'});
 
@@ -76,56 +89,57 @@ $('.menuFloatDescription').mouseover('click touchstart', function() {
 $('.menuFloatStreetview').mouseover('click touchstart', function() {
 	$('#bgContainer').animate({top: '6%', left: "0%", height: '88%', width: "100%", }, 300);
 	$('#bgContainer').animate({opacity: '1'}, 300);
-	$('.site_Title').animate({bottom: '1%',}, 300); 
-	$('.siteText').animate({top: '100%',}, 300); 
+	// $('.site_Title').animate({bottom: '1%',}, 300); 
+	// $('.siteText').animate({top: '100%',}, 300); 
 	$('.streetView').animate({opacity: '1'}, 800); 
-	$('.listItineraries').animate({opacity: '0'}, 300);
+
 	$(this).css({"text-decoration":"underline", "font-style": "italic"});
 	$('.menuFloatDescription').css({"text-decoration":"none", "font-style": "normal"});
 	$('.menuFloatInt').css({"text-decoration":"none", "font-style": "normal"});
 	$('.menuFloatVisit').css({"text-decoration":"none", "font-style": "normal"});
 	$('.streetView').css({"z-index": '10'});
 	$('.directions').css({"z-index": '-10'});
-	$('.close').animate({opacity: '1'}, 300); 
+	
 
 	$(this).css({"text-decoration":"underline", "font-style": "italic"});
 	$('.menuFloatDescription').css({"text-decoration":"none", "font-style": "normal"});
 	$('.menuFloatInt').css({"text-decoration":"none", "font-style": "normal"});  
 	$('.menuFloatVisit').css({"text-decoration":"none", "font-style": "normal"});
+	$('.menuFloatView').css({"text-decoration":"none", "font-style": "normal"});
 });
 
 $('.menuFloatVisit').mouseover('click touchstart', function() {  
 	$('#bgContainer').animate({width: '100%', height: "100%", top: "0%", left:"0%"}, 300);  
 	$('#bgContainer').animate({opacity: '1'}, 300); 
-	$('.site_Title').animate({bottom: '1%',}, 300); 
-	$('.siteText').animate({top: '100%',}, 300);
+	// $('.site_Title').animate({bottom: '1%',}, 300); 
+	// $('.siteText').animate({top: '100%',}, 300);
 	$('.directions').animate({opacity: '1'}, 800); 
 	$('.streetView').animate({opacity: '0'}, 300); 
-	$('.listItineraries').animate({opacity: '0'}, 300); 
 	$('.streetView').css({"z-index": '-10'});
 	$('.directions').css({"z-index": '10'});
-	$('.close').animate({opacity: '1'}, 300); 
 
 
 	$(this).css({"text-decoration":"underline", "font-style": "italic"});
 	$('.menuFloatStreetview').css({"text-decoration":"none", "font-style": "normal"});
 	$('.menuFloatDescription').css({"text-decoration":"none", "font-style": "normal"}); 
-	$('.menuFloatInt').css({"text-decoration":"none", "font-style": "normal"});      
+	$('.menuFloatInt').css({"text-decoration":"none", "font-style": "normal"});  
+	$('.menuFloatView').css({"text-decoration":"none", "font-style": "normal"});    
 });
 
 $('.close').on('click touchstart', function() { 
-	$('#bgContainer').animate({top: '6%', left: "6%", height: '88%', width: "88%", }, 300);
-	$('#bgContainer').animate({opacity: '1'}, 300);
+	// $('#bgContainer').animate({top: '6%', left: "6%", height: '88%', width: "88%", }, 300);
+	// $('#bgContainer').animate({opacity: '1'}, 300);
 	$('.site_Title').animate({bottom: '1%',}, 300); 
 	$('.siteText').animate({top: '100%',}, 300);
 	$(this).animate({opacity: '0'}, 300);  
-	$('.streetView').css({"z-index": '-10'});
-	$('.directions').css({"z-index": '-10'});
+	// $('.streetView').css({"z-index": '-10'});
+	// $('.directions').css({"z-index": '-10'});
 
 	$('.menuFloatStreetview').css({"text-decoration":"none", "font-style": "normal"});
 	$('.menuFloatDescription').css({"text-decoration":"none", "font-style": "normal"}); 
 	$('.menuFloatInt').css({"text-decoration":"none", "font-style": "normal"});    
 	$('.menuFloatVisit').css({"text-decoration":"none", "font-style": "normal"});
+	$('.menuFloatView').css({"text-decoration":"none", "font-style": "normal"});
 });
 
 $('.home').click('click touchstart', function() {
